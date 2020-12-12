33d1e78fe28adc5b0053f677ffd62adcf4fe6bb3
Carlo Pearson <cpearson@newrelic.com>
2020-12-11 16:26:08 -0800

    nicer routes

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ const metricsMiddleware = require('./metrics')
 
 const app = express()
 
-const PORT = 9321
+const PORT = 9123
 
 app.use(metricsMiddleware())
 
 app.get('/', (req, res) => {
-  res.send('Chrimbus!')
+  res.send('Route1')
 })
 
-app.get('/monolith', (req, res) => {
-  res.send('yup')
+app.get('/two', (req, res) => {
+  res.send('Route2')
 })
 
 app.listen(PORT, () => {
